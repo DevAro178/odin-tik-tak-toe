2b03b2ff74cb5d7ea8cbbea627e8df38ab5a91ff
Muhammad Ammad <me.ammad1786@gmail.com>
2024-09-24 20:30:31 +0500

    UI layout is done - alerts modal is pending for now I have to write bot option selection logic and I have to remove console.logs for messages in controller and switch them to alerts or dialog

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const O=`<svg class="zero" viewBox="0 0 256 256" id="Flat" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M128,236A108,108,0,1,1,236,128,108.12225,108.12225,0,0,1,128,236Zm0-192a84,84,0,1,0,84,84A84.09522,84.09522,0,0,0,128,44Z" />
+                    </svg>`;
+
+const X=`<svg class="cross" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M195.2 195.2a64 64 0 0 1 90.496 0L512 421.504 738.304 195.2a64 64 0 0 1 90.496 90.496L602.496 512 828.8 738.304a64 64 0 0 1-90.496 90.496L512 602.496 285.696 828.8a64 64 0 0 1-90.496-90.496L421.504 512 195.2 285.696a64 64 0 0 1 0-90.496z" />
+                    </svg>`
+
 const gameBoard=(function(){
     const board=['','','','','','','','',''];
     const getBoard=function(){
@@ -77,3 +87,34 @@ const controller=(function(){
     }
     return {add,markEntry}
 })();
+
+// -------------------------------------- UI ------------------------------------------
+
+let boxes=document.querySelector('div.board')
+let board=(function(){
+    const createBoard=()=>{
+        for(let j=0;j<9;j++){
+            let box=document.createElement('div');
+            box.dataset.id=j;
+            boxes.appendChild(box);
+        }
+    }
+
+    const resetBoard=()=>{
+        boxes.innerHTML='';
+        createBoard();
+    }
+
+    const addIcon=(i,icon)=>{
+        let temp=document.querySelector(`.board div[data-id="${i}"]`);
+        temp.innerHTML=icon;
+    }
+
+    return {createBoard,resetBoard, addIcon}
+})();
+board.createBoard();
+
+
+boxes.addEventListener('click',(e)=>{
+    controller.markEntry(human,e.target.dataset.id)
+});
